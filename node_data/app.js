61c80c8572fa3ff02b88ba7cbba19c7f2ba8a6c0
doubleyong <yangyangkl123>
2020-08-05 14:12:19 +0800

    8-5

--- node_data/app.js
+++ node_data/app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express(); //返回一个服务
 
 // cors 跨域
+
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,token,Content-Type");
@@ -14,6 +15,7 @@ app.all('*', function (req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
+
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -28,8 +30,17 @@ app.post("/user/login", (request, response) => {
     // 判断用户名，密码是否正确,登录成功，获取用户数据,这里不操作数据库了，直接模拟返回的数据
     var userInfo = {id: 7, userName: "sunshine", userHeader: "Cat.png", RoleId: 1} // 用户相关的数据
 //根据用户的角色id，去检查菜单数据(树形结构) -- 注：菜单==模块
-    /*menuId：菜单id, menuName：模块名称, menuUrl: 模块地址（路由），pathname:路径名，menuImgClass:菜单对应class名称，
-     menuState：菜单状态 0 有效，1无效，menuChilds：子菜单列表，数组类型*/
+    /*
+     menuId: 2,//菜单id
+     menuName: "用户管理理",//菜单名称
+     menuUrl: "/index/user",//菜单的路径，注如果子菜单要显示在父菜单中，那路径一定要有父级的路径
+     pathname: "userlist",//菜单路径名称，暂时没用
+     componentPath: "user/UserManger",//对应组件的路径
+     menuImgClass: 'TeamOutlined',//菜单对应的class名称,主要用于设置菜单的ICON
+     menuState: "0",//菜单的状态，0 有效，1无效
+     isContainChildren:false,//是否包含子组件中
+     menuChilds: []	//子菜单列表，必须是数组类型
+     */
 
     var menuInfo = [
         {
@@ -39,8 +50,32 @@ app.post("/user/login", (request, response) => {
             pathname: "userlist",
             componentPath: "user/UserManger",
             menuImgClass: 'TeamOutlined',
+            pId:0,
             menuState: "0",
-            menuChilds: []
+            isContainChildren:false,
+            menuChilds: [{
+                menuId: 10,
+                menuName: "添加用户",
+                menuUrl: "/index/adduser",
+                pathname: "adduser",
+                componentPath: "user/AddUser",
+                menuImgClass: 'VideoCameraAddOutlined',
+                pId:2,
+                menuState: "0",
+                isContainChildren:false,
+                menuChilds: []
+            },{
+                menuId: 11,
+                menuName: "修改用户",
+                menuUrl: "/index/modifyUser",
+                pathname: "modifyUser",
+                componentPath: "user/ModifyUser",
+                menuImgClass: 'VideoCameraAddOutlined',
+                pId:2,
+                menuState: "0",
+                isContainChildren:false,
+                menuChilds: []
+            }]
         },
         {
             menuId: 3,
@@ -49,7 +84,9 @@ app.post("/user/login", (request, response) => {
             pathname: "role",
             componentPath: "user/RoleManger",
             menuImgClass: 'WhatsAppOutlined',
+            pId:0,
             menuState: "0",
+            isContainChildren:true,
             menuChilds: [
                 {
                     menuId: 7,
@@ -58,19 +95,22 @@ app.post("/user/login", (request, response) => {
                     pathname: "addrole",
                     componentPath: "user/AddRole",
                     menuImgClass: 'VideoCameraAddOutlined',
+                    pId:3,
                     menuState: "0",
-                    menuChilds: [
-                        {
-                            menuId: 8,
-                            menuName: "角色详情",
-                            menuUrl: "/index/role/addrole/roleInfo",
-                            pathname: "roleInfo",
-                            componentPath: "user/RoleInfo",
-                            menuImgClass: 'TagOutlined',
-                            menuState: "0",
-                            menuChilds: []
-                        }
-                    ]
+                    isContainChildren:false,
+                    menuChilds: []
+                },
+                {
+                    menuId: 8,
+                    menuName: "角色详情",
+                    menuUrl: "/index/role/roleInfo",
+                    pathname: "roleInfo",
+                    componentPath: "user/RoleInfo",
+                    menuImgClass: 'TagOutlined',
+                    isContainChildren:false,
+                    pId:3,
+                    menuState: "0",
+                    menuChilds: []
                 },
                 {
                     menuId: 9,
@@ -79,41 +119,15 @@ app.post("/user/login", (request, response) => {
                     pathname: "rolelist",
                     componentPath: "user/RoleList",
                     menuImgClass: 'StarOutlined',
+                    pId:3,
                     menuState: "0",
+                    isContainChildren:false,
                     menuChilds: []
                 }
             ]
         }
     ];
 
-    /*
-     var menuInfo=[{menuId: 1, menuName: "菜品管理", menuUrl: "/Dish",pathname:"Dish",componentPath:"Dish",menuImgClass: 'el-icon-dish', menuState: "0",
-     menuChilds: [{menuId: 9, menuName: "菜品设置", menuUrl: "/DishSetList",pathname:"DishSetList", componentPath:"DishSetList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 10, menuName: "菜品类型", menuUrl: "/DishTypeList",pathname:"DishTypeList", componentPath:"DishTypeList",menuImgClass: '', menuState: "0", menuChilds: []}
-     ]},
-     { menuId: 2, menuName: "运营管理", menuUrl: "/Business",pathname:"Business", componentPath:"Business",menuImgClass: 'el-icon-files', menuState: "0",
-     menuChilds: [{menuId: 11, menuName: "营业管理", menuUrl: "/BusinessList",pathname:"BusinessList", componentPath:"BusinessList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 12, menuName: "餐桌管理",menuUrl:"/diningTableList",pathname:"diningTableList",componentPath:"diningTableList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 13, menuName: "会员管理", menuUrl: "/MemberList",pathname:"MemberList", componentPath:"MemberList",menuImgClass: '', menuState: "0", menuChilds: []}]},
-     { menuId: 3, menuName: "库存管理", menuUrl: "/Consumables",pathname:"Consumables", componentPath:"Consumables",menuImgClass: 'el-icon-box', menuState: "0",
-     menuChilds: [{menuId: 14, menuName: "消耗品管理", menuUrl: "/ConsumablesList",pathname:"ConsumablesList", componentPath:"ConsumablesList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 15, menuName: "消耗品类型", menuUrl: "/ConsumablesTypeList",pathname:"ConsumablesTypeList",componentPath:"ConsumablesTypeList",menuImgClass: '', menuState: "0", menuChilds: []}
-     ]},
-     { menuId: 4, menuName: "订单管理", menuUrl: "/OrderList",pathname:"OrderList", componentPath:"OrderList",menuImgClass: 'el-icon-tickets', menuState: "0",menuChilds: []},
-     { menuId: 5, menuName: "基础信息管理", menuUrl: "/Staff",pathname:"Staff", componentPath:"",menuImgClass: 'el-icon-user', menuState: "0",
-     menuChilds: [{menuId: 16, menuName: "员工管理", menuUrl: "/StaffList",pathname:"StaffList", componentPath:"StaffList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 17, menuName: "角色管理", menuUrl: "/RoleList",pathname:"RoleList", componentPath:"RoleList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 18, menuName: "权限管理", menuUrl: "/PowerList",pathname:"PowerList", componentPath:"PowerList",menuImgClass: '', menuState: "0", menuChilds: []},{menuId: 19, menuName: "模块管理", menuUrl: "/ModuleList",pathname:"ModuleList", componentPath:"ModuleList",menuImgClass: '', menuState: "0", menuChilds: []}]},
-     { menuId: 6, menuName: "系统设置", menuUrl: "/System",pathname:"System", componentPath:"System",menuImgClass: 'el-icon-setting', menuState: "0",
-     menuChilds: [{menuId: 20, menuName: "数据备份", menuUrl: "/DataBackupList",pathname:"DataBackupList", componentPath:"DataBackupList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 21, menuName: "操作日志",menuUrl:"/OplogList",pathname:"OplogList",componentPath:"OplogList",menuImgClass: '', menuState: "0", menuChilds: []}]},
-     { menuId: 7, menuName: "财务分析", menuUrl: "/Financial",pathname:"Financial", componentPath:"Financial",menuImgClass: 'el-icon-bank-card', menuState: "0",
-     menuChilds: [{menuId: 22, menuName: "财务管理", menuUrl: "/FinancialList",pathname:"FinancialList", componentPath:"FinancialList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 23, menuName: "成本管理",menuUrl:"/CostList",pathname:"CostList",componentPath:"CostList",menuImgClass: '', menuState: "0", menuChilds: []},
-     {menuId: 24, menuName: "其他支出",menuUrl:"/OtherList",pathname:"OtherList",componentPath:"OtherList",menuImgClass: '', menuState: "0", menuChilds: []}]},
-     { menuId: 8, menuName: "回收站", menuUrl: "/WasteList",pathname:"WasteList", componentPath:"WasteList",menuImgClass: 'el-icon-delete', menuState: "0",menuChilds: []}
-     ]
-     */
 //返回用户数据格式， data:登录用户数据，与权限菜单数据
 // token:登录成功后，返回给用户的token值
 // returnCode: 返回登录的状态码，200表示登录成功
